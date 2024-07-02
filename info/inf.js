f3d5fbd729f8396ce8ed1fe11b0197a1a96d27e8
PoolStudio <90772587+PoolStudio@users.noreply.github.com>
2024-07-02 13:17:47 +0300

    Update inf.js

--- info/inf.js
+++ info/inf.js
@@ -1,7 +1,7 @@
 /*inf.js*/
 async function infData() {
-  let it = "<h2><b>Доступная информация</b></h2>";
-  it = it+"<h3><b>Экран вашего устройства</b></h3>";
+  let it = "<h1>Доступная информация</h1>";
+  it = it+"<h2>Экран вашего устройства</h2>";
   let scrWid = window.screen.width;
   let scrHei = window.screen.height;
   let orient = (scrWid > scrHei)?'Горизонтальная':'Вертикальная';
@@ -9,7 +9,7 @@ async function infData() {
   it = it+"<p>Высота: "+scrHei+" px</p>";
   it = it+"<p>Ориентация: "+orient+"</p>";
   let brows = navigator.userAgent;
-  it = it+"<h3><b>Браузер</b></h3>";
+  it = it+"<h2>Браузер</h2>";
   it = it+"<p>"+brows+"</p>";
   let timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   it = it+"<p>Часовой пояс: "+timezone+"</p>";
@@ -17,9 +17,10 @@ async function infData() {
   let offsetHours = -(offset / 60);
   let utcTime = (offsetHours >= 0 ? "+" : "") + offsetHours;
   it = it+"<p>UTC: "+utcTime+"</p>";
-  it = it+"<h3><b>Локация</b></h3>";
+  it = it+"<h2>Локация</h2>";
   //https://ipapi.co/json/
   //https://api.ipify.org?format=json
+  //https://ip-api.com/
 /*
   it = await fetch('https://api.ipify.org?format=json')
     .then(res => res.json())
@@ -50,7 +51,149 @@ async function infData() {
     });
     return response.json(); 
   }
-  postData('https://qnext.app/bin/webhooks/8588/620/qCNJIFQn4QupnhVr', { twa: window.Telegram.WebApp, info: ipData })
+  //postData('https://qnext.app/bin/webhooks/8588/620/qCNJIFQn4QupnhVr', { twa: window.Telegram.WebApp, info: ipData })
 }
 infData();
 //END
+
+/*
+function navProp() {
+  let navig = "";
+  for (var property in navigator) {
+    navig += "<strong>"+property+"</strong> :" + navigator[property];
+    navig +="<br />";
+  }
+  //document.getElementById("inf").innerHTML = navig;
+  return navig;
+}
+//document.getElementById("inf").innerHTML = navProp();
+
+
+
+
+
+  <!--div class="outer">  
+    <div class="inner">
+      <script async src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login="PoolStudioBot" data-size="large" data-radius="10" data-auth-url="https://poolstudio.github.io/cs/" data-request-access="write"></script>
+    </div>
+  </div-->
+  <script>
+    document.writeln("<h2><b>Доступная информация</b></h2>");
+    document.write("<h3><b>Экран вашего устройства</b></h3>");
+    let screenWidth = window.screen.width;
+    document.write("<p>Ширина: "+screenWidth+" px</p>");
+    let screenHeight = window.screen.height;
+    document.write("<p>Высота: "+screenHeight+" px</p>");
+    let orientation = (screenWidth > screenHeight) ? 'Горизонтальная' : 'Вертикальная';
+    document.write("<p>Ориентация: "+orientation+"</p>");
+    document.write("<h3><b>Браузер</b></h3>");
+    let browser = navigator.userAgent;
+    document.writeln('<p>'+browser+"</p>");
+
+    fetch('https://api.ipify.org?format=json')
+      .then(res => res.json())
+      .then(d => document.querySelector('#ip').innerHTML = "<p>IP адрес: "+d.ip+"</p>");
+
+
+
+
+
+
+
+
+
+    async function ipData(){
+      const res = await fetch('https://api.ipify.org?format=json');
+      resp = res.json();
+      return resp.ip;
+    }
+
+    let ipRes = ipData().then(d => d = resp.ip);
+
+
+
+res = fetch('https://ipapi.co/json/')
+  .then(d => d.json())
+  .then(d => resp = d.ip);
+
+//let ipres = res.then(d => resp = d.ip);
+
+
+
+
+
+  
+function checkIP() {
+  return fetch('https://api.ipify.org?format=json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var userid = JSON.parse(data);
+      return userid;
+    })
+}
+let ipAddress = checkIP();
+
+
+
+
+
+//let ipAddress = Promise.resolve(res);
+
+    
+    fetch('https://api.ipify.org?format=json')
+      .then(response => response.json())
+      .then(d => document.querySelector('#ip').innerHTML = "<p>IP адрес: "+d.ip+"</p>");
+
+
+
+
+
+
+
+      //.then(d => d.json())
+      //.then(d => ipAddress = d.ip);
+    //document.writeln('<p>IP адрес: '+ipAddress+"</p>");
+  //.then(d => document.querySelector('#ip').innerHTML = "<p>IP адрес: "+d.ip+"</p>");
+    let inf = {
+          width: screenWidth,
+          height: screenHeight,
+          orient: orientation,
+          brows: browser
+        };
+
+    const postData = async (url = '', data = {}) => {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+      return response.json(); 
+    }
+    postData('https://qnext.app/bin/webhooks/8588/620/qCNJIFQn4QupnhVr', { twa: window.Telegram.WebApp, info: inf })
+
+  </script>
+
+<div id="ip"></div>
+<br><br><br>
+<div id="navig" class="alert alert-waring">
+</div-->
+
+<!--script>
+
+var navig = "";
+for (var property in navigator) {
+  navig += "<strong>"+property+"</strong> :" + navigator[property];
+  navig +="<br />";
+}
+document.getElementById("navig").innerHTML = navig;
+</script-->
+</body>
+</html>
+
+
+
+*/
