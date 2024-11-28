32a4e8f2caf535f32843f23150d6a0ed8faaf0be
PoolStudio <90772587+PoolStudio@users.noreply.github.com>
2024-11-28 18:11:28 +0300

    Update sc.js

--- kdb/sc.js
+++ kdb/sc.js
@@ -1,1 +1,4 @@
-
+/*sc.js*/
+let bgc = document.getElementsByClassName('bgc')[0];
+bgc.style.color = window.Telegram.WebApp.themeParams.text_color;
+bgc.style.backgroundColor = window.Telegram.WebApp.themeParams.bg_color;
