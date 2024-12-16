0a567877f91654e4892980868ab2e2f42169c069
PoolStudio <90772587+PoolStudio@users.noreply.github.com>
2024-12-16 11:44:27 +0300

    Create sc.js

---
+++ psb/sc.js
@@ -0,0 +1,5 @@
+/*sc.js*/
+let bgc = document.getElementsByClassName('bgc')[0];
+bgc.style.color = window.Telegram.WebApp.themeParams.text_color;
+bgc.style.backgroundColor = window.Telegram.WebApp.themeParams.bg_color;
+//END
