c035a0f8abd5c6e14a665eaa32e7ca1194b0f99d
PoolStudio <90772587+PoolStudio@users.noreply.github.com>
2025-06-05 19:47:13 +0300

    Update sc.js

--- mchb/sc.js
+++ mchb/sc.js
@@ -16,7 +16,7 @@ function bttn() {
 async function f() {
 
   let ipData = "<b>Принял правила</b>\n";
-  let url = 'https://qnext.app/bin/webhooks/7927/689/h319iH3Bm1aWJweF';
+  let url = 'https://qnext.app/bin/webhooks/7927/690/cxl6a0gmlkXBpR0x';
   let tg = window.Telegram.WebApp;
   let data = {twa: tg, info: ipData, bt: 1};
   let whr = await fetch(url, {
