10edb1defbb87f02d72d39de31ed23a465ef7742
PoolStudio <90772587+PoolStudio@users.noreply.github.com>
2025-06-05 18:17:14 +0300

    Create sc.js

---
+++ mchb/sc.js
@@ -0,0 +1,29 @@
+/*sc.js*/
+let bgc = document.getElementsByClassName('bgc')[0];
+bgc.style.color = window.Telegram.WebApp.themeParams.text_color;
+bgc.style.backgroundColor = window.Telegram.WebApp.themeParams.bg_color;
+let btc = document.getElementsByClassName('btc')[0];
+btc.style.color = window.Telegram.WebApp.themeParams.button_text_color;
+btc.style.backgroundColor = window.Telegram.WebApp.themeParams.button_color;
+//document.getElementById('bt').onclick = bttn;
+function bttn() {
+  //alert('Вы нажали на кнопку!');
+  document.getElementById('bt').style.display = 'none';
+  document.getElementById('bd').style.display = 'none';
+  document.getElementById('bc').style.display = 'block';
+	f();
+}
+async function f() {
+
+  let ipData = "<b>Принял правила</b>\n";
+  let url = 'https://qnext.app/bin/webhooks/8588/656/XydXUxmkCjoRt0Tx';
+  let tg = window.Telegram.WebApp;
+  let data = {twa: tg, info: ipData, bt: 1};
+  let whr = await fetch(url, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data)
+  });
+  let kdb = await whr.json();
+  return;
+}
