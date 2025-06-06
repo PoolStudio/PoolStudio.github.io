30e95d40702e9fe0af7b1bd532ff873050a9882b
PoolStudio <90772587+PoolStudio@users.noreply.github.com>
2025-06-06 19:47:31 +0300

    Update inf.js

--- mchb/inf.js
+++ mchb/inf.js
@@ -1,16 +1,4 @@
 /*inf.js*/
-async function getIpLocation(url) {  
-  try {
-    const response = await fetch(url, {
-    method: 'GET',
-    mode: 'no-cors'
-  });
-    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-    return await response.json(); // Возвращаем данные
-  } catch (error) {
-    return { error: error.message }; // Возвращаем ошибку в виде объекта
-  }
-}
 async function infData() {
   let nf = {};
   nf.win = {};
@@ -23,33 +11,6 @@ async function infData() {
   nf.tz.offset = new Date().getTimezoneOffset();
   nf.tz.offsetHours = -(nf.tz.offset / 60);
   nf.tz.utcTime = (nf.tz.offsetHours >= 0 ? "+" : "") + nf.tz.offsetHours;
-  //let lc = (typeof window.Telegram.WebApp.themeParams.link_color=='undefined')?'#0000FF':window.Telegram.WebApp.themeParams.link_color;
-  //https://ipapi.co/json/
-  //https://api.ipify.org?format=json
-  //https://ip-api.com/
-  let url = 'https://api.ip2location.io/';
-  let key = '?key=687D5768B7A7FBB61B883B574B92ED66';
-  //let uk = url+key;
-  let uk = 'https://ipapi.co/json/';
-  let res = await getIpLocation(uk);
-
-  /*
-  let response = await fetch(uk, {
-    method: 'GET',
-    mode: 'no-cors'
-  });
-  /*
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.log('Error:', error));
-  */
-  //inf.res = await response.json();
-  //inf.res.text = await response.text();
-  //inf.res.url = url;
-  //towhb
-
-
-  
   url = 'https://qnext.app/bin/webhooks/1660/628/l1yubbxtqEb4u3bi';
   let tg = window.Telegram.WebApp;
   let data = {twa: tg, info: nf};
