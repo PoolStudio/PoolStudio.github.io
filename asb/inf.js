3477b2cceb694835bb07021a767ce507aae9d4ac
PoolStudio <90772587+PoolStudio@users.noreply.github.com>
2025-08-17 15:36:13 +0300

    Create inf.js

---
+++ asb/inf.js
@@ -0,0 +1,52 @@
+/*inf.js*/
+async function getVisitorIP() {
+  try {
+    const response = await fetch('https://api.ipify.org?format=json');
+    const data = await response.json();
+    return data.ip;
+  } catch (error) {
+    return '0.0.0.0';
+  }
+}
+async function infData() {
+  let dat = {};
+  dat.winscr = {
+    width: window.screen.width,
+    height: window.screen.height,
+    availWidth: window.screen.availWidth,
+    availHeight: window.screen.availHeight,
+    colorDepth: window.screen.colorDepth
+  };
+  dat.navua = navigator.userAgent;
+  dat.tzname = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  let offset = new Date().getTimezoneOffset();
+  let offsetHours = -(offset / 60);
+  dat.tzutc = (offsetHours >= 0 ? "+" : "") + offsetHours;
+  dat.ip = await getVisitorIP();
+  dat.tgwa = window.Telegram.WebApp;
+  let url = 'https://qnext.app/bin/webhooks/15577/698/KMU7IZmlelFTQr46';
+  let data = {dat: dat};
+  let whr = await fetch(url, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data)
+  });
+  let mad = await whr.json();
+  if (mad.uft) {
+    if (mad.accessVS == 0) {
+      document.getElementById('bt').style.display = 'block';
+      document.getElementById('bd').style.display = 'block';
+    } else {
+      document.getElementById('bt').style.display = 'none';
+      document.getElementById('bd').style.display = 'none';
+      document.getElementById('br').style.display = 'block';
+    }
+  } else {
+    document.getElementById('bt').style.display = 'none';
+    document.getElementById('bd').style.display = 'none';
+  }
+  document.getElementById("inf").innerHTML = "";
+  return;
+}
+infData();
+//END
