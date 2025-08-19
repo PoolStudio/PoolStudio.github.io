b03c29401f14537eca3b310833cfde06488d0df9
PoolStudio <90772587+PoolStudio@users.noreply.github.com>
2025-08-19 17:04:39 +0300

    Update inf.js

--- asb/inf.js
+++ asb/inf.js
@@ -33,7 +33,7 @@ async function infData() {
   });
   let mad = await whr.json();
   if (mad.uft) {
-    if (mad.acsbt == 0) {
+    if (mad.acsbt) {
       document.getElementById('bt').style.display = 'block';
       document.getElementById('bd').style.display = 'block';
     } else {
@@ -50,4 +50,3 @@ async function infData() {
 }
 infData();
 //END
-
